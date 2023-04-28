adeb9ef7317ff9f741bac1cb6f39c541ccc23212
Ayush <ayushkumar9601@gmail.com>
2023-04-28 22:16:00 +0530

    Repaired

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homePage, name='home'),
-    path('update/<str:pk>/', views.updatePage, name='update'),
+    path('edit/<str:pk>/', views.updatePage, name='edit'),
     path('delete/<str:pk>/', views.deletePage, name='delete'),
 ]

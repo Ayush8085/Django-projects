bd9c2879eb44908ed1d5f729a618e5f8513d5130
Ayush <ayushkumar9601@gmail.com>
2023-05-02 23:31:41 +0530

    placeholder added

--- todo_list/base/forms.py
+++ todo_list/base/forms.py
@@ -3,6 +3,8 @@ from django.forms import ModelForm
 from .models import Tasks
 
 class TasksForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter your task here'}))
+
     class Meta:
         model = Tasks
         fields = '__all__'
